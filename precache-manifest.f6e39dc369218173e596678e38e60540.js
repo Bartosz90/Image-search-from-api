e2769cffaee22e84ab19aa8d6e45ce8017c08e66
Bartek Krupa <krupa1.bk@gmail.com>
2019-07-18 20:21:28 +0200

    Updates

---
+++ precache-manifest.f6e39dc369218173e596678e38e60540.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae71099301f113042d2dd138b85f7f1",
+    "url": "/Image-search-from-api/index.html"
+  },
+  {
+    "revision": "ce7e9168e086424ccdd1",
+    "url": "/Image-search-from-api/static/css/main.9e6248cb.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Image-search-from-api/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ce7e9168e086424ccdd1",
+    "url": "/Image-search-from-api/static/js/main.d8579727.chunk.js"
+  },
+  {
+    "revision": "a1d1f9578b7dd3e25237",
+    "url": "/Image-search-from-api/static/js/runtime~main.ec8e8218.js"
+  }
+]);
